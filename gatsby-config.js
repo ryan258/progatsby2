0541bcdcb1527bc919a0217263a3af122400b4d2
Ryan Johnson <ryan@258webco.com>
2018-11-21 12:30:47 -0600

    add uglify-webpack-plugin to get netlify-cms to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-netlify`, // make sure to put last in the array
   ],
 }
